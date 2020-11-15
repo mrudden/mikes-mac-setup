d78eb23c1ea4645905f9d3cd3a2d27f7d12e3cfe
Michael Rudden <michael@michaelrudden.com>
2020-11-15 05:03:57 -0500

    commenting things out and testing array

--- mac-setup.sh
+++ mac-setup.sh
@@ -3,28 +3,32 @@
 echo "Starting setup procedure!"
 
 # Install Homebrew if not installed
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew update
+#brew update
 
 # Brew formulae
-brew_formulae=()
+brew_formulae=(test1 test2 test3)
 # Brew Cask formulae
-brew_cask_formulae=()
+brew_cask_formulae=(test1 test2 test3)
 
 # Pseudocode: for formula in brew_formulae: brew install $formula
+for i in "${brew_formulae[@]}"
+do
+	echo $i
+done
 
 # Pseudocode: for formula in brew_cask_formulae: brew cask install $formula
 
 # Java notes for homebrew - from "brew info java"
 #For the system Java wrappers to find this JDK, symlink it with
-sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+#sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 #openjdk is keg-only, which means it was not symlinked into /usr/local,
 #because it shadows the macOS `java` wrapper.
 
 #If you need to have openjdk first in your PATH run:
-echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+#echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
 
 #For compilers to find openjdk you may need to set:
-export CPPFLAGS="-I/usr/local/opt/openjdk/include"
+#export CPPFLAGS="-I/usr/local/opt/openjdk/include"
